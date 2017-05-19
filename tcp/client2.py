5ecffa01a69517cb4346ead1471d145ae511258c
Syed Rafay <rafay_syed1@yahoo.com>
2017-05-19 16:34:58 +0500

    Sending file name encoded as bytes and decoding it while receiving and saving it with the same name on the server as it was when client sent it

--- tcp/client2.py
+++ tcp/client2.py
@@ -1,20 +1,23 @@
 import socket               # Import socket module
-
+i=0;
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                 # Reserve a port for your service.
 
 s.connect((host, port))
 
-file_Name = input("Enter the name of file you want to send(with extension) : ");
+fileName = input("Enter the name of file you want to send(with extension) : ");
                   
-f = open(file_Name,'rb')
+f = open(fileName,'rb')
 print ("Sending...");
+s.send(fileName.encode('utf-8'));
 l = f.read(1024)            # Read into buffer for sending
 while (l):
-    print ('Sending...');
+    i+=1
+    print ('Sending...'+str(i));
     s.send(l)
     l = f.read(1024)
+
 f.close()
 print ("Done Sending");
 s.shutdown(socket.SHUT_WR)
